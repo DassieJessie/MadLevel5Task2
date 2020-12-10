eb66153130677a756bb54c015a54f1faafab04f9
Jesse <sadsa@dsadas.nl>
2020-12-10 18:59:13 +0100

    RV Adapter & Data class

---
+++ GameBacklog/app/src/main/java/com/example/gamebacklog/ui/GameAdapter.kt
@@ -0,0 +1,39 @@
+package com.example.gamebacklog.ui
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.gamebacklog.R
+import com.example.gamebacklog.model.Game
+import kotlinx.android.synthetic.main.item_game.view.*
+
+abstract class GameAdapter(private val games: List<Game>) :
+    RecyclerView.Adapter<GameAdapter.ViewHolder>() {
+
+    inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+
+        fun databind(game: Game) {
+            itemView.tvTitle.text = game.title
+            itemView.tvPlatform.text = game.platform
+            itemView.tvReleaseDate.text = itemView.resources.getString(R.string.release,
+                game.releaseDate.dayOfMonth.toString(),
+                game.releaseDate.month.name,
+                game.releaseDate.year.toString())
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        return ViewHolder(
+            LayoutInflater.from(parent.context).inflate(R.layout.item_game, parent, false)
+        )
+    }
+
+    override fun getItemCount(): Int {
+        return games.size
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.databind(games[position])
+    }
+}
