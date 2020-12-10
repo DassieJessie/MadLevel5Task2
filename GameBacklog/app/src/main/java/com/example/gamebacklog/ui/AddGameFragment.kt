c8404a23611889066fd3ff2a3e1dba9b64db7215
Jesse <sadsa@dsadas.nl>
2020-12-10 16:15:37 +0100

    refactoring

--- GameBacklog/app/src/main/java/com/example/gamebacklog/ui/AddGameFragment.kt
+++ GameBacklog/app/src/main/java/com/example/gamebacklog/ui/AddGameFragment.kt
@@ -1,31 +1,27 @@
-package com.example.gamebacklog
+package com.example.gamebacklog.ui
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
-import androidx.navigation.fragment.findNavController
+import com.example.gamebacklog.R
 
 /**
  * A simple [Fragment] subclass as the second destination in the navigation.
  */
-class SecondFragment : Fragment() {
+class AddGameFragment : Fragment() {
 
     override fun onCreateView(
             inflater: LayoutInflater, container: ViewGroup?,
             savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_second, container, false)
+        return inflater.inflate(R.layout.fragment_add_game, container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        view.findViewById<Button>(R.id.button_second).setOnClickListener {
-            findNavController().navigate(R.id.action_SecondFragment_to_FirstFragment)
-        }
     }
 }
